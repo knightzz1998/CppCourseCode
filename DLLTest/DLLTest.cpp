462e72f0ce8a47b824029e25d4f5cc3422965d95
王天赐 <15565946702@163.com>
2021-12-01 18:20:37 +0800

    DLL 动态库的创建与隐式链接

---
+++ DLLTest/DLLTest.cpp
@@ -0,0 +1,17 @@
+﻿// DLLTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+#include "MyDLL.h"
+
+int main()
+{
+    cout << "Hello World!\n";
+    int x = fnMyDll();
+    cout << "x : " << x << endl;
+
+    CMyDll myDll;
+    int result = myDll.add(1, 2);
+    cout << "result : " << result << endl;
+}
--- MyDLL/MyDLL/MyDLL.cpp
+++ MyDLL/MyDLL/MyDLL.cpp
@@ -1,7 +1,7 @@
 // 定义DLL中的导出函数
 
-// #include "pch.h" // 预编译头文件, 之前版本的是 stdafx.h , 属性 => 配置属性 => C++ => 预编译头
-#include "MyDll.h"
+#include "pch.h" // 预编译头文件, 之前版本的是 stdafx.h , 属性 => 配置属性 => C++ => 预编译头
+#include "MyDLL.h"
 
 // 定义导出变量
 MYDLL_API int nMyDll = 10;
